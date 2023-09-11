078fc4277c00dc7e4f2e9e8ebf237ed6da9f758d
Devesh Krishan <deveshkrishan2003@gmail.com>
2023-09-12 01:46:37 +0530

    made user table

--- myApp/src/backend/algorizz_db.sql
+++ myApp/src/backend/algorizz_db.sql
@@ -1,1 +1,10 @@
 -- test
+
+CREATE TABLE "User"
+(
+	Username VARCHAR(20) NOT NULL,
+    [Password] VARCHAR(30) NOT NULL,
+	Email VARCHAR(60) PRIMARY KEY NOT NULL,
+	Active BIT NULL, -- Possible feature where you can add friends and see active users
+	Phone VARCHAR(15) NULL --Can use this for 2FA in the future
+)
